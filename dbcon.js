9ac6cf0ebdd6ff9d8a7916d9a2eecf2993043006
rafael my master <sorafaelbook@gmail.com>
2024-01-31 14:24:22 -0600

    world message database

--- dbcon.js
+++ dbcon.js
@@ -4,7 +4,6 @@ const log = console.log
 
 
 module.exports = dbcon = () => {
-
     mongoose.connect(process.env.MONGO_URI)
     .then( () => log("db connected !"))
     .catch( error => log(error))
--- index.js
+++ index.js
@@ -16,5 +16,6 @@ app.get("/", (req, res) => res.send("API OF DUNGEON BORN"))
 app.use("/users", require('./routes/userR'))
 app.use("/characters", require('./routes/characterR'));
 app.use("/quests", require('./routes/questR'));
+app.use("/worldmessage", require('./routes/worldMessageR'));
 
 app.listen(PORT, () => log("Server is running..."))
